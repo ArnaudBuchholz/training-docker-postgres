cbc499671ec0e0f48a9a018088aa0f64a63d5983
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-10-07 16:36:23 -0400

    Creating record

--- index.js
+++ index.js
@@ -17,39 +17,96 @@ async function main () {
         console.log('Connected')
         done(null, conn)
       }
-    }
+    },
+    debug: true
   })
+
+  const TABLENAMES = {
+    USERS: 'users'
+  }
  
-  const usersTableExists = await knex.schema.hasTable('users')
-  console.log('users :', usersTableExists)
+  const usersTableExists = await knex.schema.hasTable(TABLENAMES.USERS)
+  if (usersTableExists) {
+    console.log('Drop users table...')
+    await knex.schema.dropTable(TABLENAMES.USERS)
+  }
+  console.log('Creating users table...')
+  await knex.schema.createTable(TABLENAMES.USERS, table => {
+    table.increments('id')
+      .primary()
+    table.enum('status', ['pending', 'active', 'locked', 'deleted'])
+      .notNullable()
+      .defaultTo('pending')
+      .index()
+
+    table.timestamp('created_at', true)
+      .defaultTo(knex.raw('now()'))
+    table.timestamp('updated_at', true)
+      .notNullable()
+      .defaultTo(knex.raw('now()'))
+    table.timestamp('deleted_at', true)
+
+    table.string('login')
+      .notNullable()
+      .index()
+    table.string('firstName')
+      .notNullable()
+    table.string('lastName')
+      .notNullable()
+
+    /* Create relation between audience and connection */
+    // table.integer('connection_id').references('connections.id');
+    table.unique(['login'])
+  })
+  console.log("Table created.")
+
+  const bookshelf = require('bookshelf')(knex)
+  const User = bookshelf.Model.extend({
+    tableName: TABLENAMES.USERS,
+    hasTimestamps: true
+  })
+
+  User.upsert = function (user) {
+    const { login, firstName, lastName } = user
+    if (!login || !firstName || !lastName) {
+      throw Error('login, firstName and lastName must be specified')
+    }
+    console.log('User.upsert', user, '...')
+    return knex(TABLENAMES.USERS)
+      .first('id')
+      .where({
+        login
+      })
+      .then(record => {
+        if (record) {
+          console.log('\trecord found, updating...')
+          return knex(TABLENAMES.USERS)
+            .update({
+              ...user,
+              updated_at: new Date()
+            })
+            .where({ id })
+        }
+        console.log('\tno record found, creating...')
+        return knex(TABLENAMES.USERS)
+          .insert(user)
+          .returning('id')
+          .then(ids => ids[0])
+      })
+      .then(value => {
+        console.log('\tdone.')
+        return value
+      })
+  }
+
+  await User.upsert({
+    login: 'abuchholz',
+    firstName: 'arnaud',
+    lastName: 'buchholz'
+  })
+
+  const arnaud = await new User({ id: 1 }).fetch()
+  console.log(arnaud.attributes)
 }
 
 main()
-
-/*
-const bookshelf = require('bookshelf')(pg)
-
-const User = bookshelf.model('User', {
-  tableName: 'users',
-  posts() {
-    return this.hasMany(Posts)
-  }
-})
-   
-const Post = bookshelf.model('Post', {
-  tableName: 'posts',
-  tags() {
-    return this.belongsToMany(Tag)
-  }
-})
-   
-const Tag = bookshelf.model('Tag', {
-  tableName: 'tags'
-})
-   
-new User({id: 1}).fetch({withRelated: ['posts.tags']}).then((user) => {
-  console.log(user.related('posts').toJSON())
-}).catch((error) => {
-  console.error(error)
-})
-*/
